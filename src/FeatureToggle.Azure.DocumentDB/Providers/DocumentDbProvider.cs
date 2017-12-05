d7f34757b641e1d6f797b5dbdf212d3a1e10067d
mikanyg <mikael@mny.dk>
2017-12-06 00:01:55 +0100

    Small improvements

--- src/FeatureToggle.Azure.DocumentDB/Providers/DocumentDbProvider.cs
+++ src/FeatureToggle.Azure.DocumentDB/Providers/DocumentDbProvider.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Azure.Documents.Client;
 
 namespace FeatureToggle.Azure.DocumentDB.Providers
 {
     public class DocumentDbProvider : IBooleanToggleValueProvider
     {
+        public static string AuthKey { get; set; }
+        public static string ServiceEndpoint { get; set; }
+
+        public static string DatabaseId { get; set; } = "FeatureToggle";
+        public static string CollectionId { get; set; } = "Toggles";
+
         public bool EvaluateBooleanToggleValue(IFeatureToggle toggle)
         {
-            throw new NotImplementedException();
+            // Get name
+
+            // Validate connection properties
+
+            // Get document
+            var client = new DocumentClient(new Uri(ServiceEndpoint), AuthKey);
+            
+            return false;
         }
     }
 }
--- src/FeatureToggle.Azure.TableStorage/Providers/TableStorageProvider.cs
+++ src/FeatureToggle.Azure.TableStorage/Providers/TableStorageProvider.cs
@@ -6,6 +6,10 @@ namespace FeatureToggle.Azure.TableStorage.Providers
 {
     public class TableStorageProvider : IBooleanToggleValueProvider
     {
+        public static string ConnectionString { get; set; }
+
+        public static string TableName { get; set; } = "FeatureToggles";
+
         public bool EvaluateBooleanToggleValue(IFeatureToggle toggle)
         {
             throw new NotImplementedException();
