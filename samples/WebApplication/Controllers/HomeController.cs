37c312041df919f366d6493e55b45220b2b6f58c
mikanyg <mikael@mny.dk>
2017-12-06 23:29:44 +0100

    Added sample solution with basic asp.net mvc site using tablestorage as featuretoggle provider

---
+++ samples/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FeatureToggle;
+using WebApplication.FeatureToggles;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = Is<FrontpageUIFeature>.Enabled ? "SURPRISE!!!" : "Getting started";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
