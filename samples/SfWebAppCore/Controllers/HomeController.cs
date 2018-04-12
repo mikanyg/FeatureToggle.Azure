33965303581fb4dc0a1644bddd6946c9f2107a72
Mikael Nygaard <mikanyg@mny.dk>
2018-04-13 00:02:44 +0200

    Added package description and meta data for all packages

---
+++ samples/SfWebAppCore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using FeatureToggle;
+using Microsoft.AspNetCore.Mvc;
+using SfWebAppCore.FeatureToggles;
+using SfWebAppCore.Models;
+
+namespace SfWebAppCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = Is<CoolNewFeatureToggle>.Enabled ? "AboutPageFeature is enabled in Service Fabric Config !!!" : "Your application description page."; 
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ samples/SfWebAppCore/FeatureToggles/CoolNewFeatureToggle.cs
@@ -0,0 +1,12 @@
+﻿using FeatureToggle.Azure.Toggles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SfWebAppCore.FeatureToggles
+{
+    public class CoolNewFeatureToggle : ServiceFabricToggle
+    {
+    }
+}
