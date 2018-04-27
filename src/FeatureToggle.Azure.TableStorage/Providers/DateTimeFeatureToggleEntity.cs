5f7f680c1e7e6f9c22d495d5b14be4a08ca0f941
mikanyg <mikael@mny.dk>
2018-04-27 23:14:53 +0200

    TableStorageProvider now implements IDateTimeToggleValueProvider

---
+++ src/FeatureToggle.Azure.TableStorage/Providers/DateTimeFeatureToggleEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FeatureToggle.Azure.Providers
+{
+    public class DateTimeFeatureToggleEntity : FeatureToggleEntity
+    {
+        public DateTimeFeatureToggleEntity() : base() { }
+
+        public DateTimeFeatureToggleEntity(string componentName, string toggleName) : base(componentName, toggleName)
+        {
+            Toggle = default(DateTime);
+        }
+
+        public DateTime Toggle { get; set; }
+    }
+}
