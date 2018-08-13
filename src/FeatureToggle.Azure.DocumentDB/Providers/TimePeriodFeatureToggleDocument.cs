f3617a4c6e73f41259e1ef3f7075e6aefb772856
mikanyg <mikael@mny.dk>
2018-08-13 22:58:48 +0200

    Document Db provider implements ITimePeriodProvider interface for time limited feature toggles

---
+++ src/FeatureToggle.Azure.DocumentDB/Providers/TimePeriodFeatureToggleDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FeatureToggle.Azure.Providers
+{
+    public class TimePeriodFeatureToggleDocument : FeatureToggleDocument
+    {
+        public TimePeriodFeatureToggleDocument() : base() { }
+
+        public TimePeriodFeatureToggleDocument(string toggleName) : base(toggleName)
+        {
+            EnabledFrom = default(DateTime);
+            EnabledTo = default(DateTime);
+        }
+
+        public DateTime EnabledFrom { get; set; }
+        public DateTime EnabledTo { get; set; }
+    }
+}
---
+++ test/FeatureToggle.Azure.DocumentDB.Test/TimePeriodToggleTest.cs
@@ -0,0 +1,55 @@
+﻿using FeatureToggle.Azure.DocumentDB.Test.Toggles;
+using FeatureToggle.Azure.Providers;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+
+namespace FeatureToggle.Azure.DocumentDB.Test
+{
+    [TestFixture]
+    public class TimePeriodToggleTest : DocumentDbTestFixture
+    {
+        [Test]
+        public async Task FeatureEnabled_LimitedTimeOfferFeatureToggleSetToEnabledTomorrow_ToggleValueIsFalse()
+        {
+            // Arrange
+            AutoCreateToggle<LimitedTimeOfferFeatureToggle>();
+            await UpdateToggleDocument(new TimePeriodFeatureToggleDocument(nameof(LimitedTimeOfferFeatureToggle)) { EnabledFrom = DateTime.Now.AddDays(1), EnabledTo = DateTime.Now.AddDays(2) });
+
+            var toggle = new LimitedTimeOfferFeatureToggle();
+            // Act
+            var toggleValue = toggle.FeatureEnabled;
+            // Assert
+            toggleValue.ShouldBeFalse();
+        }
+
+        [Test]
+        public async Task FeatureEnabled_LimitedTimeOfferFeatureToggleSetToExpiredYesterday_ToggleValueIsFalse()
+        {
+            // Arrange
+            AutoCreateToggle<LimitedTimeOfferFeatureToggle>();
+            await UpdateToggleDocument(new TimePeriodFeatureToggleDocument(nameof(LimitedTimeOfferFeatureToggle)) { EnabledFrom = DateTime.Now.AddDays(-2), EnabledTo = DateTime.Now.AddDays(-1) });
+
+            var toggle = new LimitedTimeOfferFeatureToggle();
+            // Act
+            var toggleValue = toggle.FeatureEnabled;
+            // Assert
+            toggleValue.ShouldBeFalse();
+        }
+
+        [Test]
+        public async Task FeatureEnabled_LimitedTimeOfferFeatureToggleSetToEnabledYesterday_ToggleValueIsTrue()
+        {
+            // Arrange
+            AutoCreateToggle<LimitedTimeOfferFeatureToggle>();
+            await UpdateToggleDocument(new TimePeriodFeatureToggleDocument(nameof(LimitedTimeOfferFeatureToggle)) { EnabledFrom = DateTime.Now.AddDays(-1), EnabledTo = DateTime.Now.AddDays(1) });
+
+            var toggle = new LimitedTimeOfferFeatureToggle();
+            // Act
+            var toggleValue = toggle.FeatureEnabled;
+            // Assert
+            toggleValue.ShouldBeTrue();
+        }
+    }
+}
